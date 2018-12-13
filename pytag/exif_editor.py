285b18a499916e99f0c7e28843dbd112e37542dd
edr <github@rochefort.de>
2018-12-13 22:04:29 +0100

    Read and write metadata

--- pytag/exif_editor.py
+++ pytag/exif_editor.py
@@ -1,9 +1,22 @@
 from pytag import cli
+import pytag.meta_data as md
+
+_field_name="Exif.Photo.UserComment"
 
 
-def read_exif_field(field_name: str, path: str) -> str:
+def _read_exif_field(field_name: str, path: str) -> str:
     std_out_lines = cli.run_cmd(["exiv2", "-K", field_name, path])
     return " ".join(std_out_lines[0].split()[3:])
 
-def write_exif_field(field_name: str, value: str, path: str):
+
+def _write_exif_field(field_name: str, value: str, path: str):
     cli.run_cmd(['exiv2', '-M', 'set ' + field_name + ' ' + value, path])
+
+
+def get_meta_data(path: str) -> md.MetaData:
+    serialized = _read_exif_field(_field_name, path)
+    return md.deserialize(serialized)
+
+
+def set_meta_data(path: str, data: md.MetaData):
+    _write_exif_field(_field_name, data.serialize(), path)
--- tests/test_exif_editor.py
+++ tests/test_exif_editor.py
@@ -2,25 +2,43 @@ import hashlib
 from unittest import TestCase
 
 from pytag import cli
+import pytag.exif_editor as ee
+from pytag.meta_data import MetaData
 
 read_file = "../res/read.jpg"
 write_file = "../res/write.jpg"
+read_file_md = "../res/read_md.jpg"
+write_file_md = "../res/write_md.jpg"
 
-import pytag.exif_editor as ee
 
 class TestExifEditor(TestCase):
 
-    def test_read_foobar(self):
-        value = ee.read_exif_field("Exif.Photo.UserComment", read_file)
+    def test_read_raw(self):
+        value = ee._read_exif_field("Exif.Photo.UserComment", read_file)
         self.assertEqual('Some string with \'ä\' and "quotes', value)
 
-    def test_write_foobar(self):
+    def test_write_raw(self):
         cli.run_cmd(["cp", read_file, write_file])
         with open(write_file, 'rb') as f:
             md5_pre = hashlib.md5(f.read()).hexdigest()
 
             self.assertEqual("011da2f7ff8114d10b35150c2e962b26", md5_pre)
-            ee.write_exif_field("Exif.Photo.UserComment", "My Dummy Value öä ' \" ", write_file)
+            ee._write_exif_field("Exif.Photo.UserComment", "My Dummy Value öä ' \" ", write_file)
             md5_post = hashlib.md5(f.read()).hexdigest()
             self.assertEqual("d41d8cd98f00b204e9800998ecf8427e", md5_post)
+
+    def test_read_md(self):
+        data = ee.get_meta_data(read_file_md)
+        self.assertEqual("42", data.ver)
+        self.assertEqual(["My Dummy Tag öä ' \" ", "tag 2"], data.tags)
+
+    def test_write_md(self):
+        cli.run_cmd(["cp", read_file_md, write_file_md])
+        with open(write_file_md, 'rb') as f:
+            md5_pre = hashlib.md5(f.read()).hexdigest()
+
+            self.assertEqual("197c10f162136a0fa984477eb911058d", md5_pre)
+            ee.set_meta_data(write_file_md, MetaData("42", ["My other Dummy Tag öä ' \" ", "tag 2"]))
+            md5_post = hashlib.md5(f.read()).hexdigest()
+            self.assertEqual("eebc838d12ba676fc6adab2d4d434889", md5_post)
 

618df57bb21afe9fabc45882cfaa3c0b07db2642
edr <github@rochefort.de>
2018-12-13 21:15:07 +0100

    Add version to meta data

--- pytag/meta_data.py
+++ pytag/meta_data.py
@@ -3,14 +3,15 @@ from typing import List
 
 
 class MetaData:
-    def __init__(self, tags: List[str]):
+    def __init__(self, ver: str, tags: List[str]):
+        self.ver = ver
         self.tags = tags
 
     def serialize(self) -> str:
-        return json.dumps({"tags": self.tags})
+        return json.dumps({"ver": self.ver, "tags": self.tags})
 
 
 def deserialize(serialized: str):
     decoded = json.loads(serialized)
-    md = MetaData(decoded["tags"])
+    md = MetaData(decoded["ver"], decoded["tags"])
     return md
--- tests/test_meta_data.py
+++ tests/test_meta_data.py
@@ -6,8 +6,8 @@ import pytag.meta_data as md
 class TestMetaData(TestCase):
 
     def test_serialize(self):
-        data = md.MetaData(["foo", "bar", "foo bar", "äöü", "'", '"'])
+        data = md.MetaData("42", ["foo", "bar", "foo bar", "äöü", "'", '"'])
         serialized: str = data.serialize()
-        self.assertEqual('{"tags": ["foo", "bar", "foo bar", "\\u00e4\\u00f6\\u00fc", "\'", "\\""]}', serialized)
+        self.assertEqual('{"ver": "42", "tags": ["foo", "bar", "foo bar", "\\u00e4\\u00f6\\u00fc", "\'", "\\""]}', serialized)
         deserialized = md.deserialize(serialized)
         self.assertEqual(data.tags, deserialized.tags)

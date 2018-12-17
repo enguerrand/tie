a18566d8a47f50bbb2bb9d56754590c73d7f2d19
edr <github@rochefort.de>
2018-12-17 20:26:21 +0100

    Add update method for single file

--- pytag/index.py
+++ pytag/index.py
@@ -1,16 +1,41 @@
 import os
 
 from pytag import cli
+import pytag.exif_editor as ee
+import pytag.symlinks as sl
 
 TAGS_DIR_NAME = "tags"
+SEPARATOR_PLACE_HOLDER = ":"
 
 
 class Index:
-    def __init__(self, root_dir):
-        self.root_dir = root_dir
-        self.tags_dir = os.path.join(self.root_dir, TAGS_DIR_NAME)
-        self._create_if_absent()
+    def __init__(self, root_dir: str, exif: ee.ExifEditor):
+        self._exif = exif
+        self._root_dir = root_dir
+        self._tags_dir = os.path.join(self._root_dir, TAGS_DIR_NAME)
+        _create_dir_if_absent(self._tags_dir)
 
-    def _create_if_absent(self):
-        cli.run_cmd(["mkdir", "-p", self.tags_dir])
+    def update(self, path: str):
+        meta_data = self._exif.get_meta_data(path)
+        for tag in meta_data.tags:
+            absolute_tag_dir_path = os.path.abspath(self._create_tag_if_absent(tag))
+            absolute_file_path = os.path.abspath(path)
+            sl.ln(absolute_file_path, self._build_link_abs_path(absolute_tag_dir_path, absolute_file_path))
+
+    def _create_tag_if_absent(self, tag: str) -> str:
+        path = os.path.join(self._tags_dir, tag)
+        _create_dir_if_absent(path)
+        return path
+
+    def _build_link_abs_path(self, abs_tagdir_path: str, abs_file_path: str):
+        return os.path.join(abs_tagdir_path, _build_link_name(abs_file_path))
+
+
+def _build_link_name(abs_file_path: str):
+        return abs_file_path.replace(os.path.sep, SEPARATOR_PLACE_HOLDER)
+
+
+def _create_dir_if_absent(path: str):
+    cli.run_cmd(["mkdir", "-p", path])
+
 
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,19 +1,35 @@
 import os
 from unittest import TestCase
 
+import pytag.exif_editor as ee
 from pytag import cli
 from pytag.index import Index
+from tests.test_tags import *
 
 TEST_INDEX_LOCATION = "../res/index"
 
 
 class TestIndex(TestCase):
 
+    def setUp(self):
+        self._remove_index()
+        self.index = Index(TEST_INDEX_LOCATION, ee.ExifEditor())
+
+    def tearDown(self):
+        self._remove_index()
+
     def test_initialization(self):
-        cli.run_cmd(["rm", "-rf", TEST_INDEX_LOCATION])
-        index = Index(TEST_INDEX_LOCATION)
         self.assertTrue(self._file_exists(TEST_INDEX_LOCATION))
         self.assertTrue(self._file_exists(os.path.join(TEST_INDEX_LOCATION, "tags")))
+
+    def test_update_file(self):
+        self.index.update("../res/read_md.jpg")
+        self.assertTrue(self._file_exists(os.path.join(TEST_INDEX_LOCATION, "tags", TEST_READ_TAG_1)))
+        self.assertTrue(self._file_exists(os.path.join(TEST_INDEX_LOCATION, "tags", TEST_READ_TAG_2)))
+
+
+    def _remove_index(self):
+        cli.run_cmd(["rm", "-rf", TEST_INDEX_LOCATION])
 
     def _file_exists(self, path: str) -> bool:
         return os.path.exists(path)

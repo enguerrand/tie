68707af522e27a2ab0883936a9fd5e46d319066a
edr <github@rochefort.de>
2018-12-22 00:30:00 +0100

    Get rid of logging in cli module

--- tie/cli.py
+++ tie/cli.py
@@ -3,7 +3,6 @@ from typing import List
 
 
 def run_cmd(words: List[str]) -> List[str]:
-    print("Running command: "+" ".join(words))
     cp = subprocess.run(words, stdout=subprocess.PIPE)
     cp.check_returncode()
     return cp.stdout.decode("utf-8").split("\n")

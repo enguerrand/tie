2d1a6283d7849dd2db1366fa46af3d667f301921
edr <github@rochefort.de>
2019-01-05 21:56:47 +0100

    Tolerate missing config file or missing values in config file

--- lib/config.py
+++ lib/config.py
@@ -3,25 +3,37 @@ import os
 import typing
 from pathlib import Path
 
+
 ENV_VAR_CONFIG_FILE = 'TIE_CONFIG_PATH'
 GENERAL_SECTION = 'GENERAL'
+
+DEFAULT_EXIF_FIELD_NAME = "Exif.Photo.UserComment"
 
 
 class Configuration:
     def __init__(self):
-        self.exif_field_name = "Exif.Photo.UserComment"
+        self.exif_field_name = DEFAULT_EXIF_FIELD_NAME
         self.index_path = os.path.join(str(Path.home()), ".tie")
 
     def _update_from_file(self):
         path = _get_config_file_path()
         ini = _read_section_less_config_file(path)
+        settings = ini[GENERAL_SECTION]
         try:
-            settings = ini[GENERAL_SECTION]
             self.exif_field_name = settings['exif_field']
+        except KeyError:
+            pass
+        try:
             self.index_path = settings['index_path']
         except KeyError:
             pass
-        print(self.exif_field_name)
+
+
+def _get_value_or_default(settings: dict, name: str, fallback: str):
+        try:
+            return settings[name]
+        except KeyError:
+            return fallback
 
 
 def _get_config_file_path() -> str:
@@ -32,15 +44,20 @@ def _get_config_file_path() -> str:
 
 
 def _read_section_less_config_file(path: str) -> configparser.ConfigParser:
-    content = _read_file_and_prepend_section_header(path)
+    section_header = '[' + GENERAL_SECTION + ']\n'
+    content = section_header + _read_file_contents(path)
     config_parser = configparser.RawConfigParser()
     config_parser.read_string(content)
     return typing.cast(configparser.ConfigParser, config_parser)
 
 
-def _read_file_and_prepend_section_header(path) -> str:
-    with open(path) as f:
-        content = '[' + GENERAL_SECTION + ']\n' + f.read()
+def _read_file_contents(path) -> str:
+    content = ""
+    try:
+        with open(path) as f:
+            content += f.read()
+    except FileNotFoundError:
+        pass
     return content
 
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,6 @@ from lib import config
 class TestConfig(TestCase):
 
     def test_config_path_default(self):
-        os.unsetenv("TIE_CONFIG_PATH")
         path = config._get_config_file_path()
         self.assertEqual(os.path.join(str(Path.home()), ".tie.ini"), path, "Default config file path")
 
@@ -30,4 +29,19 @@ class TestConfig(TestCase):
         c = config.load_user_config()
         self.assertEqual("Vendor.What.Ever", c.exif_field_name, "Wrong exif field name read from file")
         self.assertEqual("/home/foo/.tie/", c.index_path, "Wrong index path read from file")
+        del os.environ["TIE_CONFIG_PATH"]
+
+    def test_config_file_missing_key(self):
+        cfp = "../res/testconfig2.ini"
+        os.environ["TIE_CONFIG_PATH"] = cfp
+        self.assertEqual(cfp, config._get_config_file_path(), "Config file path from environment variable")
+        c = config.load_user_config()
+        self.assertEqual("Exif.Photo.UserComment", c.exif_field_name, "Default exif field name")
+        self.assertEqual("/home/foo/.tie/", c.index_path, "Wrong index path read from file")
+        del os.environ["TIE_CONFIG_PATH"]
+
+    def test_config_file_not_found(self):
+        os.environ["TIE_CONFIG_PATH"] = "/file/that/does/not/exist.tie"
+        c = config.load_user_config()
+        self.assertEqual(config.DEFAULT_EXIF_FIELD_NAME, c.exif_field_name, "Failed to fall back to default config")
         del os.environ["TIE_CONFIG_PATH"]

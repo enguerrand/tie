18a9cd70a885ae28b1a7504e89d1825eea22775e
edr <github@rochefort.de>
2019-01-06 00:30:30 +0100

    print usage

--- lib/options_parser.py
+++ lib/options_parser.py
@@ -3,6 +3,77 @@ from typing import List
 
 from lib.query import MatchType
 
+USAGE_STRING = """
+    usage: tie.py action [options] [tags] [files]
+    
+    
+    ACTIONS:
+        help:   print this help
+        query:  query for files with specified tags
+        list:   list tags of specified file(s)
+        tag:    add specified tag to specified file(s)
+        untag:  remove specified tag from specified file(s)
+        clear:  clear all tags from specified file(s)
+        index:  update the index for the specied file(s)
+    
+    All actions may be called with their full name or just their initial character.
+    In other words, the following to commands are equivalent:
+    
+        tie.py help
+        tie.py h
+    
+    
+    OPTIONS:
+        -f, --files
+            All subsequent non-option-arguments will be interpreted as file specifications.
+            If this option is omitted and the specified action requires a file specification, the
+            last non-option argument will be treated as the file specification.
+                        
+        -F, --frontend batch|cli|gtk
+            Chooses the frontend. The default is cli (command line interface).
+            The batch frontend is non-interactive mode suitable for scripts.
+            The gtk interface requires a running X session and gtk.
+            
+        -m, --match-type all|any
+            Only applicable for the query action. Specifies whether to query for file that contain all or any of the
+            specified tags, respectively.
+    
+    
+    Depending on the chosen action, tags must be specified. All non-option-arguments that are provided after the
+    action argument and before the files specification are interpreted as tags.
+    
+    
+    EXAMPLES:
+        Querying:
+            tie query 'tag 1' tag2
+
+        Querying with short form for action:
+            tie q 'tag 1' tag2
+        
+        Interactive querying:
+            tie query --frontend gtk
+        
+        Listing tags
+            tie list /path/to/file
+        
+        Tagging one file
+            tie tag 'tag 1' tag2/path/to/file1
+            
+        Tagging multiple files with the same tags
+            tie tag 'tag 1' tag2 --files /path/to/file1 [/path/to/file2..]
+        
+        Untagging
+            tie untag 'tag 1' tag2 --files /path/to/file1 [/path/to/file2..]
+        
+        Clearing all tags
+            tie clear --files /path/to/file1 [/path/to/file2..]
+        
+        Updating the index
+            tie index -f|--files /path/to/file1 [/path/to/file2..]
+        
+    
+"""
+
 
 class ParsingStage(Enum):
     action = 1
@@ -11,15 +82,17 @@ class ParsingStage(Enum):
 
 
 class Action(Enum):
-    query = 1
-    list = 2
-    tag = 3
-    untag = 4
-    clear = 5
-    index = 6
+    help = 1
+    query = 2
+    list = 3
+    tag = 4
+    untag = 5
+    clear = 6
+    index = 7
 
 
 _short_actions = {
+    'h': Action.help,
     'q': Action.query,
     'l': Action.list,
     't': Action.tag,
--- tie.py
+++ tie.py
@@ -5,11 +5,10 @@ import sys
 
 from lib import config, tie_main
 from lib import frontend_factory as ff
-from lib.config import Configuration
 from lib.exif_editor import ExifEditor
 from lib.exit_codes import EXIT_CODE_PARSE_ERROR, EXIT_CODE_FILE_NOT_FOUND
 from lib.index import Index
-from lib.options_parser import RunOptions, ParseError
+from lib.options_parser import RunOptions, ParseError, USAGE_STRING, Action
 from lib.printing import printerr
 from lib.tie_core import TieCoreImpl
 
@@ -20,13 +19,17 @@ def setup_sys_path():
     sys.path.append(basedir)
 
 
+def print_usage():
+    print(USAGE_STRING)
+
+
 def main(*args):
     try:
         setup_sys_path()
 
         configuration = config.load_user_config()
 
-        run_options = RunOptions(list(args[1:]))  # TODO: help action / option and print_usage
+        run_options = RunOptions(list(args[1:]))
 
         frontend_type = run_options.frontend
         front_end = ff.from_type(frontend_type)
@@ -37,10 +40,14 @@ def main(*args):
 
         core = TieCoreImpl(exif, index)  # TODO: inject frontend for user interaction
 
-        tie_main.run(core, run_options, front_end)
+        if run_options.action == Action.help:
+            print_usage()
+        else:
+            tie_main.run(core, run_options, front_end)
 
     except ParseError as parse_error:
         printerr("Error: " + parse_error.msg)
+        print_usage()
         sys.exit(EXIT_CODE_PARSE_ERROR)
     except FileNotFoundError:
         # No need to print it. this is already done by subprocess

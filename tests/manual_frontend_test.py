94c9e214b53b6d8b9afd4a77cf64dbd2b33b65b1
edr <github@rochefort.de>
2019-01-09 22:30:03 +0100

    Extend manual frontend tests

--- tests/manual_frontend_test.py
+++ tests/manual_frontend_test.py
@@ -3,17 +3,26 @@ from typing import List
 from lib.abstract_frontend import Frontend
 from lib.frontend_batch import FrontendBatch
 from lib.frontend_cli import FrontendCli
+from lib.printing import printerr
+
+
+def test_frontend_user_confirm(frontend: Frontend):
+    choice = frontend.get_user_confirmation("Confirm?")
+    printerr("Your choice: "+str(choice))
 
 
 def test_frontend_get_tags(frontend: Frontend, tags_choice: List[str]):
     choice = frontend.get_tags(tags_choice)
-    print("Your choice:")
+    printerr("Your choice:")
     for t in choice:
-        print(t)
+        printerr(t)
 
 
-print("Testing batch frontend")
+printerr("Testing batch frontend")
 test_frontend_get_tags(FrontendBatch(), ["foo", "bar", "foo bar"])
+test_frontend_user_confirm(FrontendBatch())
 
-print("Testing CLI frontend")
+printerr("Testing CLI frontend")
 test_frontend_get_tags(FrontendCli(), ["foo", "bar", "foo bar", "äöl"])
+test_frontend_user_confirm(FrontendCli())
+

ea2007fbb2ab35ceddc243a796c2631abc894759
edr <github@rochefort.de>
2019-01-09 22:54:30 +0100

    Add batch frontend to factory

--- lib/frontend_factory.py
+++ lib/frontend_factory.py
@@ -1,4 +1,5 @@
 from lib.abstract_frontend import Frontend
+from lib.frontend_batch import FrontendBatch
 from lib.frontend_cli import FrontendCli
 from lib.options_parser import FrontendType
 
@@ -6,4 +7,6 @@ from lib.options_parser import FrontendType
 def from_type(frontend_type: FrontendType) -> Frontend:
     if frontend_type == FrontendType.cli:
         return FrontendCli()
+    elif frontend_type == FrontendType.batch:
+        return FrontendBatch()
     raise NotImplementedError("Frontend Type "+frontend_type.name+" is not implemented yet!")
